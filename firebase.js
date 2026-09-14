8b2ccef79d6da60665c50564fcd5a0a1774cec4d
seaweedvicki-hub <seaweedvicki@gmail.com>
2026-09-15 03:51:20 +0800

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -7,16 +7,15 @@ const firebaseConfig = {
 firebase.initializeApp(firebaseConfig);
 const db = firebase.firestore();
 
-
-// 🟩 新：每個學生獨立資料
-function saveRecord(name, data){
+// 存資料
+function saveRecord(name,data){
   db.collection("students")
     .doc(name)
     .collection("records")
     .add(data);
 }
 
-// 🟩 新：讀該學生資料
+// 讀資料
 async function loadReport(name){
   let snap = await db.collection("students")
     .doc(name)

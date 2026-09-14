98fb54be54a80e16f17ea792f4e6c9a5ad891c13
seaweedvicki-hub <seaweedvicki@gmail.com>
2026-09-15 01:23:05 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,12 +1,22 @@
-self.addEventListener("install", e=>{
-  e.waitUntil(
-    caches.open("app").then(cache=>{
-      return cache.addAll([
-        "./",
-        "./index.html",
-        "./app.js",
-        "./style.css"
-      ]);
-    })
+const CACHE_NAME = "phonics-app-v1";
+
+const urlsToCache = [
+  "./",
+  "./index.html",
+  "./app.js",
+  "./manifest.json"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
   );
 });
